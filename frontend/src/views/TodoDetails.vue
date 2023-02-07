cb96af864b6c724393837f1e78f5666d9f7b7fe3
Saeverin <hsd.ch@bluewin.ch>
2023-02-07 15:51:44 +0100

    Unterseiten für ToDos

---
+++ frontend/src/views/TodoDetails.vue
@@ -0,0 +1,36 @@
+<template>
+    <ion-page>
+      <ion-header :translucent="true">
+        <ion-toolbar>
+          <ion-title>ToDo Details</ion-title>
+        </ion-toolbar>
+      </ion-header>
+  
+      <ion-content :fullscreen="true">
+        <ion-header collapse="condense">
+          <ion-toolbar>
+            <ion-title size="large">Blank</ion-title>
+          </ion-toolbar>
+        </ion-header>
+
+        ToDo Details
+        <br>
+        ID: {{ id }}
+        
+
+      </ion-content>
+    </ion-page>
+  </template>
+  
+  <script setup lang="ts">
+  import { IonButton, IonList, IonItem, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue'; 
+  import {useRoute} from 'vue-router';
+
+  const route = useRoute();
+
+  const id = route.params.id;
+  </script>
+  
+  <style scoped>
+  
+  </style>
