9e109ca9dce4831a206e67ef85fbe2e6307d42b3
Saeverin <hsd.ch@bluewin.ch>
2023-02-07 16:09:53 +0100

    BackButton hinzugefügt

--- frontend/src/views/TodoDetails.vue
+++ frontend/src/views/TodoDetails.vue
@@ -1,36 +1,47 @@
 <template>
-    <ion-page>
-      <ion-header :translucent="true">
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-buttons slot="start">
+        <ion-back-button></ion-back-button>
+        </ion-buttons>
+        <ion-title>ToDo Details</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title>ToDo Details</ion-title>
+          <ion-title size="large">Blank</ion-title>
         </ion-toolbar>
       </ion-header>
-  
-      <ion-content :fullscreen="true">
-        <ion-header collapse="condense">
-          <ion-toolbar>
-            <ion-title size="large">Blank</ion-title>
-          </ion-toolbar>
-        </ion-header>
 
-        ToDo Details
-        <br>
-        ID: {{ id }}
-        
-
-      </ion-content>
-    </ion-page>
-  </template>
+      ToDo Details
+      <br />
+      ID: {{ id }}
+    </ion-content>
+  </ion-page>
+</template>
   
   <script setup lang="ts">
-  import { IonButton, IonList, IonItem, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue'; 
-  import {useRoute} from 'vue-router';
+import {
+  IonBackButton,
+  IonButtons,  
+  IonButton,
+  IonList,
+  IonItem,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/vue";
+import { useRoute } from "vue-router";
 
-  const route = useRoute();
+const route = useRoute();
 
-  const id = route.params.id;
-  </script>
+const id = route.params.id;
+</script>
   
   <style scoped>
-  
-  </style>
+</style>
