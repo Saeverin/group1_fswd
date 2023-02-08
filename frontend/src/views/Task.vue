1f5860b45c376237b89a6b64c9334369cbb72342
ziegldom <ziegldom@students.zhaw.ch>
2023-02-08 14:54:41 +0100

    Frontend Grid

--- frontend/src/views/Task.vue
+++ frontend/src/views/Task.vue
@@ -12,27 +12,77 @@
         </ion-toolbar>
       </ion-header>
       <ion-list>
-        <ion-item button :router-link="'/tabs/task/'+task.id" :key="task.id" v-for="task in tasks">
+        <ion-row>
+          <ion-col>Title</ion-col>
+          <ion-col>Done?</ion-col>
+          <ion-col>Category</ion-col>
+          <ion-col>Project</ion-col>
+        </ion-row>
+        <ion-item
+          button
+          :router-link="'/tabs/task/' + task.id"
+          :key="task.id"
+          v-for="task in tasks"
+        >
           <ion-grid>
             <ion-row>
               <ion-col>
                 {{ task.title }}
               </ion-col>
               <ion-col>
-                <ion-button color="danger" v-if="!task.done && !task.archived"
-                  @click="finishTask(task)">Finish</ion-button>
-                <ion-button color="success" v-if="task.done && !task.archived"
-                  @click="archiveTask(task)">Archive</ion-button>
+                <ion-button
+                  color="danger"
+                  v-if="!task.done && !task.archived"
+                  @click="finishTask(task)"
+                  >Finish</ion-button
+                >
+                <ion-button
+                  color="success"
+                  v-if="task.done && !task.archived"
+                  @click="archiveTask(task)"
+                  >Archive</ion-button
+                >
               </ion-col>
+              <ion-col>
+                <!--{{ task.category }}-->
+                Kategorie d. Tasks
+              </ion-col>
+              <ion->
+                <!--{{ task.project }}-->
+                Zugehöriges Projekt
+              </ion->
             </ion-row>
           </ion-grid>
         </ion-item>
       </ion-list>
       <ion-item>
-        <ion-input type="text" placeholder="New Task Title" v-model="newTask.title"></ion-input>
+        <ion-input
+          type="text"
+          placeholder="New Task Title"
+          v-model="newTask.title"
+        ></ion-input>
       </ion-item>
       <div padding>
         <ion-button @click="addTask()">Add New Task</ion-button>
+      </div>
+
+      <div>
+        <ion-button @click="setOpen(true)">Create Task (Neu)</ion-button>
+        <!-- MODAL -->
+
+        <ion-modal :is-open="isOpen">
+          <ion-header>
+            <ion-toolbar>
+              <ion-title>Modal</ion-title>
+              <ion-buttons slot="end">
+                <ion-button @click="setOpen(false)">Close</ion-button>
+              </ion-buttons>
+            </ion-toolbar>
+          </ion-header>
+          <ion-content>
+            <p>ASDF TEST TEST INHALT MODAL</p>
+          </ion-content>
+        </ion-modal>
       </div>
     </ion-content>
   </ion-page>
@@ -51,10 +101,35 @@ import {
   IonItem,
   IonList,
   IonButton,
+  IonButtons,
+  IonModal,
   IonInput,
 } from "@ionic/vue";
 import { useTasks } from "../composables/useTasks";
+import { defineComponent, ref } from "vue";
 
-const { newTask, tasks, getTasks, addTask, finishTask, archiveTask } = useTasks();
+defineComponent({
+  components: {
+    IonButtons,
+    IonButton,
+    IonModal,
+    IonHeader,
+    IonContent,
+    IonToolbar,
+    IonTitle,
+  },
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  methods: {
+    setOpen(isOpen: boolean) {
+      this.isOpen = isOpen;
+    },
+  },
+});
 
+const { newTask, tasks, getTasks, addTask, finishTask, archiveTask } =
+  useTasks();
 </script>
