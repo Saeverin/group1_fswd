d560fadbec1e24b6a460189eb37311495cf44b4a
Saeverin <hsd.ch@bluewin.ch>
2023-02-08 14:22:25 +0100

    TaskEndpoint angepasst

--- backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/boundary/TaskEndpoint.java
+++ backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/boundary/TaskEndpoint.java
@@ -13,10 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import ch.zhaw.sml.iwi.meng.leantodo.controller.ToDoController;
+import ch.zhaw.sml.iwi.meng.leantodo.controller.TaskController;
 import ch.zhaw.sml.iwi.meng.leantodo.entity.ProjectTask;
 import ch.zhaw.sml.iwi.meng.leantodo.entity.SingleTask;
-import ch.zhaw.sml.iwi.meng.leantodo.entity.ToDo;
+import ch.zhaw.sml.iwi.meng.leantodo.entity.Task;
 
 @RestController
 @CrossOrigin
@@ -31,7 +31,6 @@ public class TaskEndpoint {
         return  taskController.listAllTasks(principal.getName());        
     }
 
-<<<<<<< HEAD:backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/boundary/ToDoEndpoint.java
     /* @RequestMapping(path = "/api/project/{id}/task", method = RequestMethod.GET)
     @PreAuthorize("isAuthenticated() AND hasRole('USER')")
     public List<ProjectTask> getProjectTasks(Principal principal) {
@@ -41,36 +40,24 @@ public class TaskEndpoint {
     @RequestMapping(path = "/api/projecttask", method = RequestMethod.POST)
     @PreAuthorize("isAuthenticated() AND hasRole('USER')")
     public void addProjectTask(@RequestBody ProjectTask projectTask, Principal principal) {
-        toDoController.persistToDo(projectTask, principal.getName());
+        taskController.persistTask(projectTask, principal.getName());
     }
     
     @RequestMapping(path = "/api/singletask", method = RequestMethod.POST)
     @PreAuthorize("isAuthenticated() AND hasRole('USER')")
     public void addSingleTask(@RequestBody SingleTask singleTask, Principal principal) {
-        toDoController.persistToDo(singleTask, principal.getName());
+        taskController.persistTask(singleTask, principal.getName());
     }
 
     @RequestMapping(path = "/api/projecttask/{id}", method = RequestMethod.PUT)
     @PreAuthorize("isAuthenticated() AND hasRole('USER')")
     public void updateProjectTask(@PathVariable("id") Long id,@RequestBody ProjectTask projectTask, Principal principal) {
-        toDoController.updateToDo(projectTask, principal.getName());
+        taskController.updateTask(projectTask, principal.getName());
     }
 
     @RequestMapping(path = "/api/singletask/{id}", method = RequestMethod.PUT)
     @PreAuthorize("isAuthenticated() AND hasRole('USER')")
     public void updateSingleTask(@PathVariable("id") Long id,@RequestBody SingleTask singleTask, Principal principal) {
-        toDoController.updateToDo(singleTask, principal.getName());
-=======
-    @RequestMapping(path = "/api/task", method = RequestMethod.POST)
-    @PreAuthorize("isAuthenticated() AND hasRole('USER')")
-    public void addTask(@RequestBody Task newTask, Principal principal) {
-        taskController.persistTask(newTask, principal.getName());
-    }
-    
-    @RequestMapping(path = "/api/task", method = RequestMethod.PUT)
-    @PreAuthorize("isAuthenticated() AND hasRole('USER')")
-    public void updateTask(@RequestBody Task task, Principal principal) {
-        taskController.updateTask(task, principal.getName());
->>>>>>> 513b507e52d3babac762efb31a42ed41fe9a0d56:backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/boundary/TaskEndpoint.java
+        taskController.updateTask(singleTask, principal.getName());
     }
 }
