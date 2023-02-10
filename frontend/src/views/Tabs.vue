4c477366a17f702df4b9f007104abfea2f6637d3
Saeverin <hsd.ch@bluewin.ch>
2023-02-10 16:44:09 +0100

    tabs angepasst

--- frontend/src/views/Tabs.vue
+++ frontend/src/views/Tabs.vue
@@ -13,9 +13,9 @@
           <ion-label>Projects</ion-label>
         </ion-tab-button>
         
-        <ion-tab-button tab="timerecord" href="/tabs/timerecord">
-          <ion-icon :icon="send" />
-          <ion-label>Timerecord</ion-label>
+        <ion-tab-button tab="archive" href="/tabs/archive">
+          <ion-icon :icon="archive" />
+          <ion-label>Archive</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
@@ -24,5 +24,5 @@
 
 <script setup lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { flash, apps, send } from 'ionicons/icons';
+import { flash, apps, archive } from 'ionicons/icons';
 </script>
